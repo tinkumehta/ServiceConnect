49e4d96ee4d5feef063f14cd7d50a9e287009885
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-10-14 13:33:59 +0530

    feat: add Vercel configuration and update server.js for CORS settings

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,10 @@ import dotenv from 'dotenv'
 
  const app = express();
 
- app.use(cors());
+ app.use(cors({
+    origin : ['http://localhost:5173'],
+    credentials : true
+ }));
  app.use(express.json());
 
  // import routes
@@ -30,8 +33,8 @@ const PORT = process.env.PORT || 5000;
 
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server running on ${PORT}`));
-    })
-    .catch((err) => console.log(err)
-    )
+    // .then(() => {
+    //     app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+    // })
+    // .catch((err) => console.log(err)
+    // )
