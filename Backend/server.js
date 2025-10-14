4e3ba99cdae5e9385d727d911abdf4880da1b289
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-10-14 13:56:07 +0530

    ff

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ import dotenv from 'dotenv'
  const app = express();
 
  app.use(cors({
-    origin : ['http://localhost:5173'],
+    origin : [ 'https://service-connect-1kdm.vercel.app','http://localhost:5173'],
     credentials : true
  }));
  app.use(express.json());
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server : {
     proxy : {
-      '/api' : 'http://localhost:8000'
+      '/api' : 'https://service-connect-five.vercel.app'
     }
   },
   plugins: [
